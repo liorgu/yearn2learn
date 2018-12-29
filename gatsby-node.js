5fd2a5fd123c12c4ea171d1754984e99d2eeb5ef
Lior Gutweter <lior.gutweter@gmail.com>
2018-12-29 13:36:48 +0200

    add optional slug field

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,7 +60,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = node.frontmatter.slug || createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
       node,

9ff0f398c1ec07f2e65fce356519928b94e0fd29
Lior Gutweter <lior.gutweter@gmail.com>
2019-01-05 14:18:42 +0200

    center the posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,19 +4,22 @@ import { DiscussionEmbed } from 'disqus-react'
 
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
+import classes from './blog-post.module.scss'
 
 const BlogPostTemplate = ({ data }) => {
   const post = data.markdownRemark
 
   return (
     <Layout>
-      <header>
-        <h1>{post.frontmatter.title}</h1>
-        <h2>{post.frontmatter.date}</h2>
-      </header>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <hr />
-      <Bio />
+      <div className={classes.contentWrapper}>
+        <header>
+          <h1>{post.frontmatter.title}</h1>
+          <h2>{post.frontmatter.date}</h2>
+        </header>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr />
+        <Bio />
+      </div>
       <hr />
       <DiscussionEmbed
         shortname="yearn2learn"
